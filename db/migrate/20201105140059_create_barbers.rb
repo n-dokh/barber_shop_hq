297d4084146daf1cde6ccce412b458803b29309d
n-dokh <natka.boichuk@gmail.com>
2020-11-05 16:10:59 +0200

    add Barbers migration

---
+++ db/migrate/20201105140059_create_barbers.rb
@@ -0,0 +1,4 @@
+class CreateBarbers < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_151909) do
+ActiveRecord::Schema.define(version: 2020_11_05_140059) do
 
   create_table "clients", force: :cascade do |t|
     t.text "name"

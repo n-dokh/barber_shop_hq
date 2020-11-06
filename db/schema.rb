b2bc1ad67f07976ac39638e3b45a552200fc508a
n-dokh <natka.boichuk@gmail.com>
2020-11-06 12:33:57 +0200

    add index view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_05_140059) do
+ActiveRecord::Schema.define(version: 2020_11_05_140060) do
+
+  create_table "barbers", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "clients", force: :cascade do |t|
     t.text "name"
